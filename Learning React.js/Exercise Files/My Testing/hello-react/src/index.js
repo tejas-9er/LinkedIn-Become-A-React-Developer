27490c69af64edfc9d3f4d51e97bf0e60fd398e5
Tejas Gandre <tgandre007@gmail.com>
2020-10-18 15:01:00 -0700

    created unordered list

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("div", {style: {color: "blue"}}, React.createElement("h1", null, "Hi!")),
+  React.createElement(
+    "ul",
+    {style: {color: "blue"}},
+    React.createElement("li", null, "Hot Dogs"),
+    React.createElement("li", null, "Burger"),
+    React.createElement("li", null, "Pizza"),
+    React.createElement("li", null, "Sushi"),
+    ),
   document.getElementById("root")
 );
 
