9e2be1949ae6eaf60707823692982d882d9cb015
Tejas Gandre <tgandre007@gmail.com>
2020-10-18 14:12:34 -0700

    added color blue to react element

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", null, "Hello!"),
-  document.getElementById('root')
+  React.createElement("h1", {style: {color: "blue"}}, "Hello!"),
+  document.getElementById("root")
 );
 
