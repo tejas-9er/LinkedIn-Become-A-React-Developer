475e88bd3ee94fa2c37fc85b0a742c78e9685700
Tejas Gandre <tgandre007@gmail.com>
2020-10-18 14:17:06 -0700

    created nested react elements

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", {style: {color: "blue"}}, "Hello!"),
+  React.createElement("div", {style: {color: "blue"}}, React.createElement("h1", null, "Hi!")),
   document.getElementById("root")
 );
 
