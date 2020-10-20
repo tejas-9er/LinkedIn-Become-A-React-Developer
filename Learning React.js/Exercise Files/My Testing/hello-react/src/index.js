a2374e5248d71be1ccb144d0c3fbb76e4bc38126
Tejas Gandre <tgandre007@gmail.com>
2020-10-20 16:58:57 -0700

    Added properties

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(){
+function Hello(props){
+  console.log(Object.keys(props));
   return (
     <div>
-      <h1>Welcome to React!</h1>
-      <p>Let's build something cool.</p>
+      <h1>Welcome to {props.library}!</h1>
+      <p>{props.message}</p>
+      <p>{Object.keys(props).length} Props Total</p>
     </div>
-  )
+  );/* Dynamic numbers for props */
 }
 
 ReactDOM.render(
-  <Hello />,
+  <Hello
+  library="React"
+  message="add dynamic data"
+  number = {3}/* This is how to write numbers */
+  />,
   document.getElementById("root")
 );
