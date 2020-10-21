4d16343d4e10f1158e512bcb2d959cd077da2ea0
Tejas Gandre <tgandre007@gmail.com>
2020-10-20 18:23:26 -0700

    passing components as properties rather than harcoding it

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-function Lake(){
+function Lake({name}){
   return(
     <div>
-      <h1>Visit Jenny Lake!</h1>
+      <h1>Visit {name}</h1>
     </div>
   );
 }
 
-function SkiResort(){
+function SkiResort({name}){
   return(
     <div>
-      <h1>Visit Jackson Mountain Resort!</h1>
+      <h1>Visit {name}</h1>
     </div>
   );
 }
 
 function App(props){
   if(props.season === "summer"){
-    return <Lake />
+    return <Lake name = "Jenny Lake!"/>
   }
   else if(props.season === "winter"){
-    return <SkiResort />
+    return <SkiResort name = "Jackson Mountain Resort!"/>
   }
 }
 
