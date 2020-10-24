abf3452a458399068e3331c3bf04d2dc4e7e7a0c
Tejas Gandre <tgandre007@gmail.com>
2020-10-24 14:57:06 -0700

    Understanding Array Destructuring

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 function Lake(){
-  return(
-    <h1>Lake</h1>
-  );
+  return <h1>Lake</h1>;
 }
 
 function SkiResort(){
-  return(
-    <h1>Ski Resort</h1>
+  return <h1>Ski Resort</h1>;
+}
+
+function App({name}){
+  return (
+    <>
+    <h1>{name}</h1>
+    <Lake />
+    <SkiResort />
+    </>
   );
 }
 
-// function App(){
-//   return (
-//     <>
-//     <Lake />
-//     <SkiResort />
-//     </>
-//   );
-// }
-
 ReactDOM.render(
-  <>
-  <Lake /> <SkiResort />
-  </>,
+  <App name = "GraphQL" />,
   document.getElementById("root")
 );
+
+const [first, second, third] =[
+  "Popcorn",
+  "Pretzels",
+  "Pineapples"
+];
+
+console.log(first);
+console.log(second);
+console.log(third);
