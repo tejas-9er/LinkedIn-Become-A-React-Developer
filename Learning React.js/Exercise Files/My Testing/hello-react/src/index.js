b4a6b39e37e29dbcc75375cf0017e76faa45ac65
Tejas Gandre <tgandre007@gmail.com>
2020-10-25 13:57:21 -0700

    another example of useState

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -2,42 +2,23 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App(){
-  const [year, setYear] = useState(2050);
-  const [manager, setManager] = useState("Alex");
-  const [status, setStatus] = useState("Open");
-  return (
-    <>
-      <div>
-        <h1>{year}</h1>
-        <button
-        onClick = {() => setYear(year + 1)}
-        >
-          New Year
-        </button>
-      </div>
-      <div>
-        <h1>Manager on Duty: {manager}</h1>
-        <button
-        onClick = {() => setManager("Rachel")}
-        >
-          New Manager
-        </button>
-      </div>
-      <div>
-        <h1>Status: {status}</h1>
-        <button onClick={() => setStatus("Open")}>Open
-        </button>
-        <button onClick={() => setStatus("Back in 5")}>Break
-        </button>
-        <button onClick={() => setStatus("Closed")}>Closed
-        </button>
-      </div>
-    </>
+function Checkbox(){
+  const [checked, setChecked] = useState(false);
+  return(
+  <>
+    <input
+    type = "checkbox"
+    value = {checked}
+    onChange={() =>
+      setChecked(checked => !checked)
+    }
+    />
+    {checked ? "checked" : "not checked"}
+  </>
   );
 }
 
 ReactDOM.render(
-  <App />,
+  <Checkbox />,
   document.getElementById("root")
 );
