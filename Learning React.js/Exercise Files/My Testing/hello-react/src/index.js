2e8346adeb44705b2b739903b7d09a82ad272fcb
Tejas Gandre <tgandre007@gmail.com>
2020-10-25 13:46:24 -0700

    using multiple state variables

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App(){
+  const [year, setYear] = useState(2050);
+  const [manager, setManager] = useState("Alex");
   const [status, setStatus] = useState("Open");
   return (
-    <div>
-      <h1>Status: {status}</h1>
-      <button onClick={() => setStatus("Open")}>Open
-      </button>
-      <button onClick={() => setStatus("Back in 5")}>Break
-      </button>
-      <button onClick={() => setStatus("Closed")}>Closed
-      </button>
-    </div>
+    <>
+      <div>
+        <h1>{year}</h1>
+        <button
+        onClick = {() => setYear(year + 1)}
+        >
+          New Year
+        </button>
+      </div>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button
+        onClick = {() => setManager("Rachel")}
+        >
+          New Manager
+        </button>
+      </div>
+      <div>
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>Open
+        </button>
+        <button onClick={() => setStatus("Back in 5")}>Break
+        </button>
+        <button onClick={() => setStatus("Closed")}>Closed
+        </button>
+      </div>
+    </>
   );
 }
 
