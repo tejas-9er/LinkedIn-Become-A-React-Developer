6fabbe6e967f8f368167a81d6a040319d53c6508
Tejas Gandre <tgandre007@gmail.com>
2020-10-25 20:03:33 -0700

    Using multiple useEffect see changes in log

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -2,27 +2,38 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Checkbox(){
-  const [checked, setChecked] = useState(false);
+function App(){
+  const[val, setVal] = useState("");
+  const[val2, setVal2] = useState("");
+
   useEffect(() => {
-    alert(`checked: ${checked.toString()}`);
+    console.log(`field 1: ${val}`);
   });
-  
+
+  useEffect(() => {
+    console.log(`field 2: ${val2}`);
+  });
+
   return(
-  <>
-    <input
-    type = "checkbox"
-    value = {checked}
-    onChange={() =>
-      setChecked(checked => !checked)
-    }
-    />
-    {checked ? "checked" : "not checked"}
-  </>
+    <>
+      <label>
+        Favorite phrase
+      </label>
+      <input
+      value = {val}
+      onChange={e => setVal(e.target.value)}/>
+      <br />
+      <label>
+        Second favorite phrase
+      </label>
+      <input
+      value = {val2}
+      onChange={e => setVal2(e.target.value)}/>
+    </>
   );
 }
 
 ReactDOM.render(
-  <Checkbox />,
+  <App />,
   document.getElementById("root")
 );
