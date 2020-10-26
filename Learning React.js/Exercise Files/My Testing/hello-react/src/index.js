16ffc84ff07ad58bd4b4105a172246f5d5b59546
Tejas Gandre <tgandre007@gmail.com>
2020-10-26 00:06:59 -0700

    Added comments on how to deploy a react app

--- Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
+++ Learning React.js/Exercise Files/My Testing/hello-react/src/index.js
@@ -24,3 +24,17 @@ ReactDOM.render(
   <Checkbox />,
   document.getElementById("root")
 );
+
+/*
+To create a production build run following command
+npm run build
+Above commands creates a build folder
+After that to serve the project on static server run following commands
+npm install -g serve (to install static server which can be used on our pc)
+
+serve -s build (serves the buld folder on localhost 5000 [The address will be visible after you run the command])
+
+Go to netlify.com
+Login with github, or other options
+Drag the build folder into netlify and it will deploy it.
+*/
